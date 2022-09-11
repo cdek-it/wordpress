8d51fbf94d8d8df04d9eb697748f7c306cbd30d2
Klem65 <ilyaklem65@gmail.com>
2022-09-11 22:05:43 +0300

    Добавлены стили для кнопки открытия карты для выбора пвз

--- templates/public/open-map.php
+++ templates/public/open-map.php
@@ -11,9 +11,29 @@
 </div>
 <style>
     .open-pvz-btn {
-        color: #7f54b3;
+        appearance: none;
+        background-color: #2ea44f;
+        border: 1px solid rgba(27, 31, 35, .15);
+        border-radius: 6px;
+        box-shadow: rgb(27 31 35 / 10%) 0 1px 0;
+        box-sizing: border-box;
+        color: #fff;
         cursor: pointer;
-        font-size: small;
+        display: inline-block;
+        font-size: 14px;
+        font-weight: 600;
+        line-height: 20px;
+        padding: 6px 16px;
+        position: relative;
+        text-align: center;
+        text-decoration: none;
+        user-select: none;
+        -webkit-user-select: none;
+        touch-action: manipulation;
+        vertical-align: middle;
+        white-space: nowrap;
+        margin-bottom: 10px;
+        margin-top: 10px;
     }
 
     #pvz-info {
