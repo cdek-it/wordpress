81d5f274f1f0d409c141109e249663a6d3884e26
Klem65 <ilyaklem65@gmail.com>
2022-11-21 16:25:28 +0300

    Скрыл поле pvz_info

--- templates/public/open-map.php
+++ templates/public/open-map.php
@@ -6,7 +6,7 @@
 ?>
 <div class="open-pvz-btn" data-layer-map="<?php echo $layerMap?>" data-postamat="<?php echo $postamat;?>">Выбрать ПВЗ</div>
 <div>
-    <input name="pvz_info" id="pvz-info" type="text">
+    <input name="pvz_info" id="pvz-info" type="hidden">
     <input name="pvz_code" id="pvz-code" type="hidden">
     <input name="city_code" id="city-code" type="hidden">
 </div>
