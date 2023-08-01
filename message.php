0d1f0b12907c19d8bcf58d23123fa906cec51cbe
Klem65 <ilyaklem65@gmail.com>
2023-08-01 10:03:52 +0300

    Изменил название тарифа заглушки

--- message.php
+++ message.php
@@ -1,4 +1,4 @@
 <?php
 define('CDEK_ERROR_DATA_NOT_FOUND', __('Данные не найдены', 'cdek'));
 define('CDEK_NO_DELIVERY_POINTS_IN_CITY', __('В этом населенном пункте (НП) доступна доставка только для тарифов "от двери". Выберите другой НП, чтобы получить доступ к тарифам "от склада".', 'cdek'));
-define('CDEK_TARIFF_PLUG_NAME', 'CDEK доставка');
+define('CDEK_TARIFF_PLUG_NAME', 'CDEK');
