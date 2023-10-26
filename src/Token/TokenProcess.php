6ecae2f295e115956bc33c5f7fabcd6f61b920c4
Klem65 <ilyaklem65@gmail.com>
2023-10-27 01:06:21 +0300

    Добавил генерацию iv для openssl

--- src/Token/TokenProcess.php
+++ src/Token/TokenProcess.php
@@ -13,10 +13,15 @@ abstract class TokenProcess
     abstract function fetchTokenFromApi();
 
     protected function encryptToken($token, $clientId) {
-        return openssl_encrypt($token, self::CIPHER, $clientId);
+        $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length(self::CIPHER));
+        $encryptedToken = openssl_encrypt($token, self::CIPHER, $clientId, 0, $iv);
+        return base64_encode($iv . $encryptedToken);
     }
 
     protected function decryptToken($token, $clientId) {
-        return openssl_decrypt($token, self::CIPHER, $clientId);
+        $data = base64_decode($token);
+        $iv = substr($data, 0, openssl_cipher_iv_length(self::CIPHER));
+        $encryptedData = substr($data, openssl_cipher_iv_length(self::CIPHER));
+        return openssl_decrypt($encryptedData, self::CIPHER, $clientId, 0, $iv);
     }
 }
