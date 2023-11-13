272d90eaab42603a555c18d869b8c6aac4da5592
Klem65 <ilyaklem65@gmail.com>
2023-11-13 15:47:18 +0300

    Убрал перезапись полей города и региона. Добавил проверку для instance

--- src/Actions/ProcessWoocommerceOrderAction.php
+++ src/Actions/ProcessWoocommerceOrderAction.php
@@ -26,28 +26,15 @@ namespace Cdek\Actions {
                 return;
             }
 
-            $api = new CdekApi;
-
             $shippingMethod = CheckoutHelper::getOrderShippingMethod($order);
-
-            $pvzInfo = CheckoutHelper::getValueFromCurrentSession('pvz_code_address');
             $pvzCode = CheckoutHelper::getValueFromCurrentSession('pvz_code');
             $tariffId = $shippingMethod->get_meta('tariff_code');
-            $cityCode = CheckoutHelper::getValueFromCurrentSession('city_code');
-
             $currency = function_exists('wcml_get_woocommerce_currency_option') ? get_woocommerce_currency() : 'RUB';
 
-            if (empty($cityCode)) {
-                $cityCode = $api->getCityCodeByCityName($order->get_billing_city(), $order->get_billing_city());
-            }
-
-            $cityData = $api->getCityByCode($cityCode);
-            $order->set_shipping_city($cityData['city']);
-            $order->set_shipping_state($cityData['region']);
-            $order->save();
-
             if (Tariff::isTariffToOffice($tariffId)) {
-                $shippingMethod->add_meta_data('pvz', $pvzCode . ' (' . $pvzInfo . ')');
+                $api = new CdekApi;
+                $pvzAddress = $api->getOffices(['code' => $pvzCode]);
+                $shippingMethod->add_meta_data('pvz', $pvzCode . ' (' . json_decode($pvzAddress)[0]->location->address . ')');
                 $shippingMethod->save_meta_data();
             }
 
@@ -58,7 +45,12 @@ namespace Cdek\Actions {
 
             OrderMetaData::addMetaByOrderId($order->get_id(), $data);
 
-            if (Helper::getActualShippingMethod($shippingMethod->get_data()['instance_id'])
+            $instance = $shippingMethod->get_data()['instance_id'];
+            if (empty($instance)) {
+                $instance = null;
+            }
+
+            if (Helper::getActualShippingMethod($instance)
                       ->get_option('automate_orders') === 'yes') {
                 wp_schedule_single_event(time() + 1, Config::ORDER_AUTOMATION_HOOK_NAME, [$order->get_id(),
                                                                                                    1]);
--- templates/public/open-map.php
+++ templates/public/open-map.php
@@ -9,4 +9,3 @@
      data-points="<?= esc_html($points) ?>">Выбрать ПВЗ
 </div>
 <input name="pvz_code" class="cdek-office-code" type="hidden">
-<input name="pvz_code_address" class="cdek-office-code-address" type="hidden">
