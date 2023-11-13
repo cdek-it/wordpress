dd84b2a7676f88f512a7246139291b6537fa05af
AlexV <a.viarvelskii@cdek.ru>
2023-11-13 14:39:36 +0700

    Исправил использование функции вычисления веса для создания заказа в СДЭК

--- src/Actions/CreateOrderAction.php
+++ src/Actions/CreateOrderAction.php
@@ -213,8 +213,7 @@ namespace Cdek\Actions {
 
             foreach ($items as $item) {
                 $product = $item->get_product();
-                $weight = $product->get_weight();
-                $weight = $this->weightCalc->getWeight($weight);
+                $weight = $this->weightCalc->getWeight($product->get_weight());
                 $quantity = (int)$item->get_quantity();
                 $totalWeight += $quantity * $weight;
                 $cost = $product->get_price();
