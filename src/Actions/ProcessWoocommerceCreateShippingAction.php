d98361aa80e008c3aff2ae5bf54338f21fbccb6d
AlexV <a.viarvelskii@cdek.ru>
2024-01-23 14:14:29 +0700

    Добавил эксепшен на отсутствие пвз отправки

--- src/Actions/ProcessWoocommerceCreateShippingAction.php
+++ src/Actions/ProcessWoocommerceCreateShippingAction.php
@@ -12,32 +12,39 @@ namespace Cdek\Actions {
     use Cdek\Helpers\CheckoutHelper;
     use Cdek\MetaKeys;
     use Cdek\Model\Tariff;
+    use RuntimeException;
     use WC_Order;
+    use WC_Order_Item_Shipping;
 
     class ProcessWoocommerceCreateShippingAction
     {
         /**
          * @throws \WC_Data_Exception
+         * @throws \Exception
          */
-        public function __invoke(\WC_Order_Item_Shipping $shipping, $package_key, $package, WC_Order $order): void
+        public function __invoke(WC_Order_Item_Shipping $shipping, $package_key, $package, WC_Order $order): void
         {
             if ($shipping->get_method_id() !== Config::DELIVERY_NAME) {
                 return;
             }
 
-            $shippingMethod = CheckoutHelper::getOrderShippingMethod($order);
-            $pvzCode = CheckoutHelper::getValueFromCurrentSession('pvz_code');
-            $tariffId = $shippingMethod->get_meta(MetaKeys::TARIFF_CODE) ?: $shippingMethod->get_meta('tariff_code');
+            $pvzCode  = CheckoutHelper::getValueFromCurrentSession('pvz_code');
+            $tariffId = $shipping->get_meta(MetaKeys::TARIFF_CODE) ?: $shipping->get_meta('tariff_code');
 
             if (Tariff::isTariffToOffice($tariffId)) {
-                $shippingMethod->add_meta_data(MetaKeys::OFFICE_CODE, $pvzCode);
-                $shippingMethod->save_meta_data();
+                if(empty($pvzCode)) {
+                    throw new RuntimeException(__('Не задан ПВЗ отправки', 'official_cdek'));
+                }
+                $shipping->add_meta_data(MetaKeys::OFFICE_CODE, $pvzCode);
+                $shipping->save_meta_data();
             }
 
-            if (Helper::getActualShippingMethod($shipping->get_instance_id())
-                      ->get_option('automate_orders') === 'yes') {
-                wp_schedule_single_event(time() + 1, Config::ORDER_AUTOMATION_HOOK_NAME, [$order->get_id(),
-                                                                                                   1]);
+            if (Helper::getActualShippingMethod($shipping->get_instance_id())->get_option('automate_orders') ===
+                'yes') {
+                wp_schedule_single_event(time() + 1, Config::ORDER_AUTOMATION_HOOK_NAME, [
+                    $order->get_id(),
+                    1,
+                ]);
             }
         }
     }
