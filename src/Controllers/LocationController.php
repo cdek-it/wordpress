f23e14c8c44624891df5d659b8bfe08fa343553a
AlexV <a.viarvelskii@cdek.ru>
2024-02-06 17:14:23 +0700

    Исправлена работа с заголовками у офисов

--- src/Controllers/LocationController.php
+++ src/Controllers/LocationController.php
@@ -9,6 +9,7 @@ namespace Cdek\Controllers {
 
     use Cdek\CdekApi;
     use Cdek\Config;
+    use WP_Error;
     use WP_REST_Request;
     use WP_REST_Response;
     use WP_REST_Server;
@@ -18,7 +19,11 @@ namespace Cdek\Controllers {
         public static function getPoints(WP_REST_Request $data): WP_REST_Response
         {
             $data = (new CdekApi)->getOffices($data->get_params());
-            return new WP_REST_Response($data instanceof \WP_Error ? $data : $data['body'], 200, $data['headers']);
+            return new WP_REST_Response($data instanceof WP_Error ? $data : $data['body'], 200, [
+                'x-current-page' => $data['headers']['x-current-page'] ?? null,
+                'x-total-pages' => $data['headers']['x-total-pages'] ?? null,
+                'x-total-elements' => $data['headers']['x-total-elements'] ?? null,
+            ]);
         }
 
         public function __invoke(): void
