2072d8324aadf12bd4edbcd715e1d3edb9827933
Stas Demin <demin.sv@cdek.ru>
2024-04-15 11:48:49 +0300

    code style fix

--- src/Checkout/InternationalOrderFields.php
+++ src/Checkout/InternationalOrderFields.php
@@ -12,45 +12,45 @@ class InternationalOrderFields implements FieldConstructorInterface
 
     public function __construct($fields = [])
     {
-        if(empty($fields)){
+        if (empty($fields)) {
             $this->fields = [
-                'passport_series' => [
-                    'label' => __('Passport Series', 'official-cdek'),
-                    'required' => true,
+                'passport_series'        => [
+                    'label'             => __('Passport Series', 'official-cdek'),
+                    'required'          => true,
                     'custom_attributes' => [
                         'maxlength' => 4,
                     ],
                 ],
-                'passport_number' => [
-                    'label' => __('Passport number', 'official-cdek'),
-                    'required' => true,
+                'passport_number'        => [
+                    'label'             => __('Passport number', 'official-cdek'),
+                    'required'          => true,
                     'custom_attributes' => [
                         'maxlength' => 6,
                     ],
                 ],
                 'passport_date_of_issue' => [
                     'type'     => 'date',
-                    'label' => __('Passport date of issue', 'official-cdek'),
+                    'label'    => __('Passport date of issue', 'official-cdek'),
                     'required' => true,
                 ],
-                'passport_organization' => [
-                    'label' => __('Passport organization', 'official-cdek'),
+                'passport_organization'  => [
+                    'label'    => __('Passport organization', 'official-cdek'),
                     'required' => true,
                 ],
-                'tin' => [
-                    'label' => __('TIN', 'official-cdek'),
-                    'required' => true,
+                'tin'                    => [
+                    'label'             => __('TIN', 'official-cdek'),
+                    'required'          => true,
                     'custom_attributes' => [
                         'maxlength' => 12,
                     ],
                 ],
                 'passport_date_of_birth' => [
                     'type'     => 'date',
-                    'label' => __('Birthday', 'official-cdek'),
+                    'label'    => __('Birthday', 'official-cdek'),
                     'required' => true,
                 ],
             ];
-        }else{
+        } else {
             $this->fields = $fields;
         }
     }
@@ -60,7 +60,7 @@ class InternationalOrderFields implements FieldConstructorInterface
     {
         $arFields = [];
 
-        foreach ($this->fields as $key => $arField){
+        foreach ($this->fields as $key => $arField) {
             $arFields[$key] = [
                 'priority' => 120,
                 'label'    => $arField['label'],
@@ -69,11 +69,11 @@ class InternationalOrderFields implements FieldConstructorInterface
                 'clear'    => true,
             ];
 
-            if(!empty($arField['type'])){
+            if (!empty($arField['type'])) {
                 $arFields[$key]['type'] = $arField['type'];
             }
 
-            if(!empty($arField['custom_attributes'])){
+            if (!empty($arField['custom_attributes'])) {
                 $arFields[$key]['custom_attributes'] = $arField['custom_attributes'];
             }
         }
