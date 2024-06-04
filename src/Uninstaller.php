df6955126f36310ec76c8b021c2c03fbcbcef255
Stas Demin <demin.sv@cdek.ru>
2024-06-04 15:14:06 +0300

    fix: unregister events

--- src/Uninstaller.php
+++ src/Uninstaller.php
@@ -9,7 +9,7 @@ class Uninstaller
     public function __invoke()
     {
         foreach (TaskManager::getTasksHooks() as $hook){
-            if (as_has_scheduled_action(Config::TASK_MANAGER_HOOK_NAME) === false) {
+            if (as_has_scheduled_action(Config::TASK_MANAGER_HOOK_NAME) !== false) {
                 wp_unschedule_event(
                     time(),
                     $hook
