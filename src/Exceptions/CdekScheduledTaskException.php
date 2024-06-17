ae7b17bfedd502e845dcfdcc7af50b840c448710
Stas Demin <demin.sv@cdek.ru>
2024-06-17 11:23:51 +0300

    fix: exception add $stopPropagation

--- src/Exceptions/CdekScheduledTaskException.php
+++ src/Exceptions/CdekScheduledTaskException.php
@@ -8,18 +8,24 @@ namespace {
 namespace Cdek\Exceptions{
 
     use WP_Error;
+    use Exception;
 
-    class CdekScheduledTaskException
+    class CdekScheduledTaskException extends Exception
     {
         public function __construct(
             string $message,
             string $code = 'cdek_error',
-            ?array $data = null
+            ?array $data = null,
+            bool $stopPropagation = true
         )
         {
-            $error = new WP_Error('cdek_error', 'Error happened at CDEKDelivery');
-            $error->add($code, $message, $data);
-            wp_die($error);
+            if($stopPropagation){
+                $error = new WP_Error('cdek_error', 'Error happened at CDEKDelivery');
+                $error->add($code, $message, $data);
+                wp_die($error);
+            }
+
+            parent::__construct($message);
         }
     }
 }
