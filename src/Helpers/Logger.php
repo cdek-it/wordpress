0e8ef097cd96149ff59ef27277e2212706115f75
Stas Demin <demin.sv@cdek.ru>
2024-12-28 14:14:55 +0300

    fix: code style fix

--- src/Helpers/Logger.php
+++ src/Helpers/Logger.php
@@ -3,15 +3,18 @@
 declare(strict_types=1);
 
 namespace {
+
     defined('ABSPATH') or exit;
 }
 
 namespace Cdek\Helpers {
+
     use WC_Logger_Interface;
 
     class Logger
     {
         private ?WC_Logger_Interface $logger = null;
+
         public function __construct()
         {
             $this->logger = wc_get_logger();
@@ -22,14 +25,14 @@ namespace Cdek\Helpers {
             return [
                 'message' => $e->getMessage(),
                 'file'    => "{$e->getFile()}:{$e->getLine()}",
-                'trace'    => $e->getTrace(),
+                'trace'   => $e->getTrace(),
             ];
         }
 
         /** @noinspection PhpUnused */
         public static function debug(string $message, array $context = []): void
         {
-            if($log = (new static())->logger){
+            if ( $log = (new static())->logger ) {
                 $log->debug($message, $context);
             }
         }
@@ -37,7 +40,7 @@ namespace Cdek\Helpers {
         /** @noinspection PhpUnused */
         public static function info(string $message, array $context = []): void
         {
-            if($log = (new static())->logger){
+            if ( $log = (new static())->logger ) {
                 $log->info($message, $context);
             }
         }
@@ -45,7 +48,7 @@ namespace Cdek\Helpers {
         /** @noinspection PhpUnused */
         public static function notice(string $message, array $context = []): void
         {
-            if($log = (new static())->logger){
+            if ( $log = (new static())->logger ) {
                 $log->notice($message, $context);
             }
         }
@@ -53,7 +56,7 @@ namespace Cdek\Helpers {
         /** @noinspection PhpUnused */
         public static function warning(string $message, array $context = []): void
         {
-            if($log = (new static())->logger){
+            if ( $log = (new static())->logger ) {
                 $log->warning($message, $context);
             }
         }
@@ -61,7 +64,7 @@ namespace Cdek\Helpers {
         /** @noinspection PhpUnused */
         public static function error(string $message, array $context = []): void
         {
-            if($log = (new static())->logger){
+            if ( $log = (new static())->logger ) {
                 $log->error($message, $context);
             }
         }
@@ -69,7 +72,7 @@ namespace Cdek\Helpers {
         /** @noinspection PhpUnused */
         public static function critical(string $message, array $context = []): void
         {
-            if($log = (new static())->logger){
+            if ( $log = (new static())->logger ) {
                 $log->critical($message, $context);
             }
         }
@@ -77,7 +80,7 @@ namespace Cdek\Helpers {
         /** @noinspection PhpUnused */
         public static function alert(string $message, array $context = []): void
         {
-            if($log = (new static())->logger){
+            if ( $log = (new static())->logger ) {
                 $log->alert($message, $context);
             }
         }
@@ -85,7 +88,7 @@ namespace Cdek\Helpers {
         /** @noinspection PhpUnused */
         public static function emergency(string $message, array $context = []): void
         {
-            if($log = (new static())->logger){
+            if ( $log = (new static())->logger ) {
                 $log->emergency($message, $context);
             }
         }
