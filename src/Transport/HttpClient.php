8e500a6fb00a489dfb33d795b4459ef61470799a
Stas Demin <demin.sv@cdek.ru>
2025-01-22 16:47:23 +0300

    fix: response check is object and method exist instead try to check implements

--- src/Transport/HttpClient.php
+++ src/Transport/HttpClient.php
@@ -109,7 +109,7 @@ namespace Cdek\Transport {
             return new HttpResponse(
                 wp_remote_retrieve_response_code($resp),
                 wp_remote_retrieve_body($resp),
-                ($headers instanceof CaseInsensitiveDictionary) ? $headers->getAll() : $headers,
+                method_exists($headers, "getAll") ? $headers->getAll() : $headers,
                 $url,
                 $method,
             );
