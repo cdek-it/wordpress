61e05679c592cbb30b87f98c5c7a7dd4ce13b5cf
Stas Demin <demin.sv@cdek.ru>
2025-02-06 11:52:38 +0300

    fix: check available shipments add notice

--- src/Validator/CheckoutValidator.php
+++ src/Validator/CheckoutValidator.php
@@ -23,35 +23,37 @@ namespace Cdek\Validator {
 
         public function __invoke(): void
         {
-            if (!WC()->cart->needs_shipping()) {
+            if ( !WC()->cart->needs_shipping() ) {
                 return;
             }
 
             $shippingMethodIdSelected = WC()->session->get('chosen_shipping_methods')[0];
 
-            if (
-                empty($shippingMethodIdSelected)
-                ||
-                strpos($shippingMethodIdSelected, Config::DELIVERY_NAME) === false
-            ) {
+            if ( empty($shippingMethodIdSelected) ) {
+                wc_add_notice(esc_html__('No shipping methods.', 'cdekdelivery'), 'error');
+
+                return;
+            }
+
+            if ( strpos($shippingMethodIdSelected, Config::DELIVERY_NAME) === false ) {
                 return;
             }
 
             $tariffCode = explode(':', $shippingMethodIdSelected)[1];
 
-            if (Tariff::isToOffice((int)$tariffCode)) {
-                if (empty(CheckoutHelper::getValueFromCurrentSession('office_code'))) {
+            if ( Tariff::isToOffice((int)$tariffCode) ) {
+                if ( empty(CheckoutHelper::getValueFromCurrentSession('office_code')) ) {
                     wc_add_notice(esc_html__('Order pickup point not selected.', 'cdekdelivery'), 'error');
                 }
             } else {
-                if (empty(CheckoutHelper::getValueFromCurrentSession('address_1'))) {
+                if ( empty(CheckoutHelper::getValueFromCurrentSession('address_1')) ) {
                     wc_add_notice(esc_html__('No shipping address.', 'cdekdelivery'), 'error');
                 }
 
                 $city   = CheckoutHelper::getValueFromCurrentSession('city');
                 $postal = CheckoutHelper::getValueFromCurrentSession('postcode');
 
-                if ((new CdekApi)->cityCodeGet($city, $postal) === null) {
+                if ( (new CdekApi)->cityCodeGet($city, $postal) === null ) {
                     wc_add_notice(
                         sprintf(/* translators: 1: Name of a city 2: ZIP code */ esc_html__(
                             'Failed to determine locality in %1$s %2$s',
@@ -67,7 +69,7 @@ namespace Cdek\Validator {
 
             $phone = CheckoutHelper::getValueFromCurrentSession('phone');
 
-            if (empty($phone)) {
+            if ( empty($phone) ) {
                 wc_add_notice(esc_html__('Phone number is required.', 'cdekdelivery'), 'error');
             } else {
                 try {
